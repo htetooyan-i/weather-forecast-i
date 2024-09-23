44c49db6f3e687ca9dee1beb94d811e801ac6366
htetooyan-i <i2406.code@gmail.com>
2024-09-24 01:39:08 +0630

    Your descriptive message here

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,50 @@
 <template>
-  <div
-    id="alert-border-2"
-    class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800"
-    role="alert"
-  >
-    <svg
-      class="flex-shrink-0 w-4 h-4"
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      viewBox="0 0 20 20"
+  <div :class="primaryBgColor">
+    <div
+      id="alert-border-2"
+      class="z-50 flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50 dark:text-red-400 dark:bg-gray-800 dark:border-red-800"
+      role="alert"
+      :class="[invalidInput ? '' : 'hidden']"
     >
-      <path
-        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
-      />
-    </svg>
-    <div class="ms-3 text-sm font-medium">
-      A simple danger alert with an
-      <a href="#" class="font-semibold underline hover:no-underline"
-        >example link</a
-      >. Give it a click if you like.
-    </div>
-    <button
-      type="button"
-      class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
-      data-dismiss-target="#alert-border-2"
-      aria-label="Close"
-    >
-      <span class="sr-only">Dismiss</span>
       <svg
-        class="w-3 h-3"
+        class="flex-shrink-0 w-4 h-4"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 14 14"
+        fill="currentColor"
+        viewBox="0 0 20 20"
       >
         <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"
         />
       </svg>
-    </button>
-  </div>
-  <div :class="primaryBgColor">
+      <div class="ms-3 text-sm font-medium">
+        Your input {{ inputName }} is not a valid city. Please enter a valid
+        city name.
+      </div>
+      <button
+        type="button"
+        class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-red-400 dark:hover:bg-gray-700"
+        data-dismiss-target="#alert-border-2"
+        aria-label="Close"
+      >
+        <span class="sr-only">Dismiss</span>
+        <svg
+          class="w-3 h-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+          />
+        </svg>
+      </button>
+    </div>
     <div class="max-w-5xl mx-auto min-h-screen px-5 pt-3">
       <header class="flex flex-rows w-full justify-between">
         <div class="max-w-md grow relative">
@@ -168,6 +167,7 @@ import { usePredictionStore } from "./stores/predict";
 import { useCountriesStore } from "./stores/countries";
 
 const inputName = ref("");
+const invalidInput = ref(false);
 
 // Define a method to toggle the unit
 const toggleUnit = () => {
@@ -246,6 +246,8 @@ const getData = () => {
       showPrediction.value = false;
       filteredName.value = [];
     } else {
+      invalidInput.value = true;
+      clearInput();
     }
   } else {
     emptyInput.value = true;
